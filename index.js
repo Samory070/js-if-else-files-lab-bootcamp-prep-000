638999e7d25714555d7800b94bbaffffd67a0a45
samory harris <samory070@gmail.com>
2018-02-13 00:46:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ if (name === "Susan") {
 } else {
     console.log("Expected the name to be Susan")
 }
-if (height === "") {
+if (height === "74") {
     console.log("The height is correct")
 } else {
-    console.log("Expected the height to be Susan")
+    console.log("Expected the height to be 74")
 }
 
 // Don't worry about this
